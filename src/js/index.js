0675720481c7af93f6aa70e16c4a7fd7d5e315d4
Slava <svstason@gmail.com>
2022-06-12 19:14:34 +0300

    have problem with pictures

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../css/style.css'
+import '../scss/style.scss'
+import '../index.html'
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,95 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { loader } = require('mini-css-extract-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
+    context: path.resolve(__dirname, 'src'),
     mode: "development",
-    entry: './src/index.js',
+    entry: './js/index.js',
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].[hash].js",
+        filename: `./js/${filename('js')}`,
+        // publicPath: '',
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
         compress: true,
+        hot: true,
         port: 3000,
     },
     plugins: [
-        new HtmlWebPlugin({ template: './src/index.html' }),
-        new CleanWebpackPlugin()
+        new HtmlWebPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: "index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, "dist/assets")
+                }
+            ]
+        })
     ],
     module: {
-        rules: [{
-            test: /\.(css|s[ac]ss)$/i,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        },
-        {
-            test: /\.(jpg|jpeg|png|svg)/,
-            use: ['file-loader']
-        }
+        rules: [
+            {
+                test: /\.html$/,
+                loader: "html-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: (resourcePath, context) => {
+                            return path.relative(path.dirname(resourcePath), context) + '/';
+                        }
+                    }
+                },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(?:|gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./img/${filename('[ext]')}`
+                    }
+                }]
+            },
+            {
+                test: /\.(?:|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./fonts/${filename('[ext]')}`
+                    }
+                }]
+            },
         ]
     }
 }
